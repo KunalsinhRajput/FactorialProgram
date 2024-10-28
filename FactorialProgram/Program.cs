ad4c80b7bd789c069dcf2b0cf70693aabd1ac376
Kunal-Rajput-18 <kunal4248rajput@gmail.com>
2024-10-28 12:45:29 +0530

    Add project files.

---
+++ FactorialProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FactorialProgram
+{
+    class Program
+    { 
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter Any Number");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            int fact = 1;
+
+            for (int i = 1; i <= num; i++)
+            { 
+                fact=fact*i;
+            }
+
+            Console.WriteLine(fact);
+
+        }
+    }
+}
